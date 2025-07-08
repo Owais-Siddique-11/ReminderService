e840ccc71b1cba189c27330c38c60b9fc65f5ef1
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2025-07-08 18:29:59 +0530

    Setup of SMTP using nodemailer done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
+const {sendBasicEmail} = require('./services/emailService')
 const startAndSetupServer = ()=>{
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended : true}));
     app.listen(3004,()=>{
         console.log(`Server running on Port ${PORT}`);
+        sendBasicEmail(
+            'admin',
+            'siddiqueowais499@gmail.com',
+            'Testing Mail',
+            'hey Sup man, what are u doing'
+        )
     })
 }
 startAndSetupServer();
