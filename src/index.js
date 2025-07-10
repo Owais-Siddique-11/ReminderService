8a19629701c7c5099a1a671c26771a649e0030a1
Owais-Siddique-11 <siddiqueowais499@gmail.com>
2025-07-10 15:13:38 +0530

    Done the Message queue

--- src/index.js
+++ src/index.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const {PORT} = require('./config/serverConfig');
-const {createChannel}=require('./utils/messageQueue');
-// const {sendBasicEmail} = require('./services/emailService')
-const jobs = require('./utils/job');
-const TicketController= require('./controllers/ticket-controller');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { PORT ,REMINDER_BINDING_KEY} = require("./src/config/serverConfig.js");
+// const { sendBasicEmail } = require("./src/services/email-service.js");
+const {subscribeMessage,createChannel}=require("./src/utils/messageQueue.js")
+// const cron = require("node-cron");
+// const TicketController=require("./src/controllers/ticket-controller.js")
+const emailService=require("./src/services/email-service.js")
+const setUpJobs=require("./src/utils/job.js")
 
-const startAndSetupServer =async ()=>{
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended : true}));
-    const channel = await createChannel();
-    app.post('/api/v1/tickets',TicketController.create );
-    app.listen(3004,()=>{
 
-        console.log(`Server running on Port ${PORT}`);
-        jobs();
-        // sendBasicEmail(
-        //     'admin',
-        //     'siddiqueowais499@gmail.com',
-        //     'Testing Mail',
-        //     'hey Sup man, what are u doing'
-        // )
-    })
-}
-startAndSetupServer();
+const setUpAndStartServer = async () => {
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+ const channel=await createChannel();
+ subscribeMessage(channel,emailService.subscribeEvents,REMINDER_BINDING_KEY)
+  app.listen(PORT, async () => {
+    // sendBasicEmail("support@admin.com","engineervele7@gmail.com","this is a testing email","hey how are you I hope you like the support")
+    console.log(`Server is listening PORT ${PORT}🚀`);
+    
+    setUpJobs();
+   
+  });
+};
+setUpAndStartServer();
